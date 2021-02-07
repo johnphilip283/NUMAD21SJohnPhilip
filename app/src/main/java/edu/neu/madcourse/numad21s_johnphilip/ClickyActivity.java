1117fb5542668807d7583508afc98dc4079c0021
John Philip <john.philip@buzzfeed.com>
2021-02-06 23:29:56 -0500

    added click handlers

--- app/src/main/java/edu/neu/madcourse/numad21s_johnphilip/ClickyActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_johnphilip/ClickyActivity.java
@@ -3,6 +3,7 @@ package edu.neu.madcourse.numad21s_johnphilip;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,6 +20,33 @@ public class ClickyActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    
+    public void onClickA(View view) {
+        setPressedValue("A");
+    }
+
+    public void onClickB(View view) {
+        setPressedValue("B");
+    }
+
+    public void onClickC(View view) {
+        setPressedValue("C");
+    }
+
+    public void onClickD(View view) {
+        setPressedValue("D");
+    }
+
+    public void onClickE(View view) {
+        setPressedValue("E");
+    }
+
+    public void onClickF(View view) {
+        setPressedValue("F");
+    }
+
+    private void setPressedValue(String val) {
+        TextView tv = (TextView) findViewById(R.id.pressedButtonName);
+        tv.setText("Pressed: " + val);
+    }
 
 }
